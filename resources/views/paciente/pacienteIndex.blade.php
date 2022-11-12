ff4bedb61e4dcc97ba48be87328ddd9aa89cd108
eduJR29 <eduardo.jimenez2912@gmail.com>
2022-11-12 11:15:54 -0600

    Implementacion de Vite

--- resources/views/paciente/pacienteIndex.blade.php
+++ resources/views/paciente/pacienteIndex.blade.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listado Pacientes</title>
+
+    @vite(['resources/css/materialize.css', 'resources/js/materialize.js',])
 </head>
 <body>
     <h1>Listado de Pacientes</h1>
